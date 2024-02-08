cc441a88a8f6a8b851daef99c193663ed1baabcd
mypace0600 <mypace0600@gmail.com>
2024-02-08 11:36:12 +0900

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+// postgresql 실행하는 방법
+brew services start postgresql@14
+psql postgres
+
+
---
+++ src/main/java/com/tutorial/spring_security/user/Role.java
@@ -0,0 +1,7 @@
+package com.tutorial.spring_security.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
